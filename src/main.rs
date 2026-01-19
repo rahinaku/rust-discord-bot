863c1d0501e7f896458c34cb1c956d1cb8851435
neto2107 <67000713+neto2107@users.noreply.github.com>
2026-01-20 02:05:37 +0900

    リクエストを送信するロジックを途中まで追加

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
+use std::env;
+
 use api_test::get_app;
+use reqwest::Client;
 
 #[tokio::main]
 async fn main() {
-    // .envファイルから環境変数を読み込む
     dotenv::dotenv().ok();
+
+    // slashコマンドを登録
+    let application_id = env::var("DISCORD_APP_ID").unwrap();
+    let token = env::var("DISCORD_TOKEN").unwrap();
+    println!("{application_id}");
+    let url = format!(
+        "https://discord.com/api/v10/applications/{}/commands",
+        application_id
+    );
+    let token = format!("Bot {}", token);
+    println!("{token}");
+    println!("{url}");
+    let client = Client::new();
+    let res = client
+        .post(url)
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .header(reqwest::header::AUTHORIZATION, token)
+        .send()
+        .await
+        .unwrap();
+    println!("{}", res.status());
+    println!("{}", res.text().await.unwrap());
+    // .envファイルから環境変数を読み込む
 
     let app = get_app();
 
